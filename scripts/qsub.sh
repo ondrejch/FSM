1e19c2ed577f72d70d90e9ca5ae7e1e8e842d0da
Ondrej Chvala <ondrejch@gmail.com>
2016-08-01 17:42:00 -0400

    job submission script

---
+++ scripts/qsub.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Jub submission script for TORQUE
+# Ondrej Chvala, ochvala@utk.edu
+# 2016-07-16
+# GNU/GPL
+
+#PBS -V
+#PBS -q fill
+#PBS -l nodes=1:ppn=8
+
+hostname
+
+module load mpi
+module load serpent
+cd ${PBS_O_WORKDIR}
+
+sss2 -omp 4 ffrr.inp
+
+# We do not need this file, safe disk pace
+# rm ffrrg.inp.out
+
+# Extract useful data
+awk 'BEGIN{ORS="\t"} /ANA_KEFF/ || /CONVERSION/ {print $7" "$8;}' ffrrg.inp_res.m > done.out
+grep ffrrg msbr.inp | sed -e s/[a-Z,\"]//g  >> done.out
+
