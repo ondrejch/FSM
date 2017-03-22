bc82a433a11c529d5d49f2031ecbb68af41cf1d3
Will Cureton <willcureton@Wills-MacBook-Pro-2.local>
2017-03-22 18:41:34 -0400

    initial commit of hexagonal core

---
+++ scripts/mk3/surfaces.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+#
+# Generate surfaces, pins, and lattice for FastDrum Serpent deck 
+# Ondrej Chvala, ochvala@utk.edu
+# 2016-08-02
+
+import math
+
+def write_surfaces(N, r, refl):
+    '''Function to write material cards for Serpent input deck.
+    Inputs: 
+        N:     size of the N x N checkerboard lattice
+        r:     radius of the fuel in the fuel pin [cm]
+        refl:  reflector thickness [cm]
+    Outputs:
+        surfaces: String containing the surface cards'''
+    
+    rclad = r + 0.1                 # Cladding outer radius              
+    rgap  = rclad +0.05             # Worst case gap for water or air
+    pitch = 4.0 * rclad + 0.01      # Lattice pitch
+    l10   = 17 * pitch / 2.0        # Radius of the cylinder bounding the lattice
+    l20   = l10 + pitch             # Radius of the cylinder bounding the lead block
+    l21   = l20 +  0.1              # Radius of the air gap cylinder
+    l22   = l21 + refl              # Radius of the steel reflector cylinder
+    fuel_rod_weight = 19.1 * math.pi * r*r * l10 # Uranium mass in each rod [g]
+
+    surfaces = '''
+%______________pins_________________________________________________
+
+pin 1   % fuel pin
+fuel   {r}
+ssteel {rclad}
+water {rgap}  % removeable: worst case flooding scenario
+lead
+
+pin 9   % lead block
+lead
+
+%______________surface definitions__________________________________
+
+surf 10 cyl 0 0 {l10} -{l10} {l10}    % Inner cylinder with the lattice
+surf 20 cyl 0 0 {l20} -{l20} {l20}    % Lead cylinder around the core
+surf 21 cyl 0 0 {l21} -{l21} {l21}    % Air gap - likely useless
+surf 22 cyl 0 0 {l22} -{l22} {l22}    % Radial reflector
+
+%______________lattice definitions_____________
+
+lat 50 2 0 0 {N} {N} {pitch}
+
+'''
+    n_fuel_rods = 0;
+    for i in range(N):          # Generates checkboard lattice
+        surfaces += (N-i)* " "
+        iodd = 0
+        if i % 2 :
+            iodd = 1
+
+        for j in range(N):
+            jodd = 0
+            r_lat = math.sqrt(math.fabs((i-(N/2.0+0.5))**2 + (j-(N/2.0+0.5))**2 - (i-(N/2.0+0.5))*(j-(N/2.0+0.5)))) # cell radius in the terms with pitch
+            if (r_lat <= 25):
+            	surfaces += "1 "
+            	n_fuel_rods = n_fuel_rods + 1
+            else:
+                surfaces += "9 "
+			
+        surfaces += "\n"
+
+    surfaces += "\n"
+    surfaces += "% Number of fuel rods = {}\n".format(n_fuel_rods)
+    surfaces += "% Uranium weight per rod [kg] = {:8.4f}\n".format(fuel_rod_weight/ 1e3)
+    surfaces += "% Total uranium weight   [kg] = {:8.4f}\n".format( n_fuel_rods * fuel_rod_weight/ 1e3)
+    
+    surfaces = surfaces.format(**locals())
+    return surfaces
+	
+if __name__ == '__main__':
+    print("This module writes surfaces, pins, and lattice for FastDrum Serpent deck.")
+    input("Press Ctrl+C to quit, or enter else to test it.")
+    print(write_surfaces(11, 1.25, 50))
---
+++ scripts/mk3/writecore.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# 
+# Write the FastDrum Serpent deck
+# Ondrej Chvala, ochvala@utk.edu
+# 2016-08-02
+
+import drumdeck
+import os
+
+import argparse
+
+# Serpent deck file name
+filename = "ffrr.inp"
+dirname  = "./"
+
+# Command line argument
+parser = argparse.ArgumentParser(description='Writes Serpent2 input deck of the Fast Cube Reactor.')
+parser.add_argument('--latsize', metavar='N', type=int, nargs='?', default=35,
+                   help='lattice size, default = 20') #, required=False)
+parser.add_argument('--fuelradius', metavar='r', type=float, nargs='?', default=1.17,
+                   help='fuel rod radius [cm], default = 1.17 cm')
+parser.add_argument('--reflector', metavar='refl', type=float, nargs='?', default=50,
+                   help='fuel rod radius [cm], default = 50 cm')
+
+
+# Parse command line arguments
+args    =  vars(parser.parse_args())
+N       = args['latsize']
+r_fuel  = args['fuelradius']
+d_refl  = args['reflector']
+
+
+# Make the deck
+s2_deck = drumdeck.write_deck(N, r_fuel, d_refl)
+fname   = dirname + filename
+print("Writing deck for lattice size ",N,", fuel radius ",r_fuel," cm, reflector thickness ",d_refl, " cm.")
+
+# Write the deck
+try:
+    f = open(fname, 'w')
+    f.write(s2_deck)
+    f.close()
+    print("Deck written,")
+except IOError as e:
+    print("Unable to write to file", fname)
+    print(e)
+
